392e706e7377d1a528c0e539ba00539ae0e86b1e
Dilla Saldia Fadillah <48883949+DillaSaldia@users.noreply.github.com>
2019-03-24 21:03:38 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main{
+	public static void main(String[] x){
+		televisi = new televisi{x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10]};
+		x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10]};
+		
+	String namatelevisi = televisi.getNamatelevisi();
+	String merk = televisi.getMerk();
+	String hargabeli = televisi.getHargabeli();
+	String hargajual = televisi.getHargajual();
+	String jenislayar = televisi.getJenislayar();
+	String jenistelevisi = televisi.getJenistelevisi();
+	String modelbarang = televisi.getModelbarang();
+	String ukuranlayar = televisi.getUkuranlayar();
+	String resolusipanel = televisi.getResolusipanel();
+	String kodebarang = televisi.getKodebarang();
+	String rasioaspek = televisi.getRasioaspek();
+	
+	System.out.println(namatelevisi);
+	System.out.println(merk);
+	System.out.println(hargabeli);
+	System.out.println(hargajual);
+	System.out.println(jenislayar);
+	System.out.println(jenistelevisi);
+	System.out.println(modelbarang);
+	System.out.println(ukuranlayar);
+	System.out.println(resolusipanel);
+	System.out.println(kodebarang);
+	System.out.println(rasioaspek);
+	}
+}	
---
+++ televisi.java
@@ -0,0 +1,99 @@
+public class televisi{
+	private String namatelevisi;
+	private String merk;
+	private String hargabeli;
+	private String hargajual;
+	private String jenislayar;
+	private String jenistelevisi;
+	private String modelbarang;
+	private String ukuranlayar;
+	private String resolusipanel;
+	private String kodebarang;
+	private String rasioaspek;
+	
+	public televisi{String namatelevisi, String merk, String hargabeli, String hargajual, String jenislayar, String jenistelevisi, 
+	String modelbarang, String ukuranlayar, String resolusipanel, String kodebarang, String rasioaspek};
+		this.namatelevisi = namatelevisi;
+		this.merk = merk;
+		this.hargabeli = hargabeli;
+		this.hargajual = hargajual;
+		this.jenislayar = jenislayar;
+		this.jenistelevisi = jenistelevisi;
+		this.modelbarang = modelbarang;
+		this.ukuranlayar = ukuranlayar;
+		this.resolusipanel = resolusipanel;
+		this.kodebarang = kodebarang;
+		this.rasioaspek = rasioaspek;
+		
+	public void setNama(String namatelevisi){
+		nama=namatelevisi;
+	}
+	public String getNama(){
+		return namatelevisi;
+	}
+	public void setMerk(String merk){
+		merk=merktelevisi;
+	}
+	public String getMerk(){
+		return merk;
+	}
+	public void setHargabeli(String hargabeli){
+		hargabeli=hargabelitelevisi;
+	}
+	public String getHargabeli(){
+		return hargabeli;
+	}
+	public void setHargajual(String hargajual){
+		hargajual=hargajualtelevisi;
+	}
+	public String getHargajual(){
+		return hargajual;
+	}
+	public void setJenislayar(String jenislayar){
+		jenislayar=jenislayartelevisi;
+	}
+	public String getJenislayar(){
+		return jenislayar;
+	}
+	public void setJenistelevisi(String jenistelevisi){
+		jenistelevisi=jenistelevisi;
+	}
+	public String getJenistelevisi(){
+		return jenistelevisi;
+	}
+	public void setModelbarang(String modelbarang){
+		modelbarang=modelbarang;
+	}
+	public String getModelbarang(){
+		return modelbarang;
+	}
+	public void setUkuranlayar(String ukuranlayar){
+		ukuranlayar=ukuranlayartelevisi;
+	}
+	public String getUkuranlayar(){
+		return ukuranlayar;
+	}
+	public void setResolusipanel(String resolusipanel){
+		resolusipanel=resolusipaneltelevisi;
+	}
+	public String getResolusipanel(){
+		return resolusipanel;
+	}
+	public void setKodebarang(String kodebarang){
+		kodebarang=kodebarangtelevisi;
+	}
+	public String getKodebarang(){
+		return kodebarang;
+	}
+	public void setRasioaspek(String rasioaspek){
+		rasioaspek=rasioaspektelevisi;
+	}
+	public String getRasioaspek(){
+		return rasioaspek;
+	}
+}
+		
+	
+	
+
+	
